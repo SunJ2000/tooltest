cb4f6ae63fa9587e40be3e0ab67f4febdb303e6b
Sunj2000 <3014137637@qq.com>
2026-01-29 16:13:55 +0800

    test

--- src/test.mjs
+++ src/test.mjs
@@ -1,12 +1,7 @@
 import { execSync } from "child_process";
 
 export function getGitDiff() {
-  try {
-    return execSync("git diff --unified=0", { encoding: "utf-8" });
-  } catch (error) {
-    console.error("执行 Git 命令失败:", error.message);
-    return "";
-  }
+  return execSync("git diff --unified=0", { encoding: "utf-8" });
 }
 
 const diff = getGitDiff();
